bfd385da530451aefb244a8cd7ba549c5d2ed892
Santigp258 <santigp258@gmail.com>
2021-03-19 22:21:46 -0500

    routes

---
+++ src/routers/AppRouter.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { JournalScreen } from '../components/journal/JournalScreen';
+import { AuthRouter } from './AuthRouter';
+export const AppRouter = () => {
+    return (
+        <Router>
+      <div>
+        <Switch>
+          <Route
+            path="/auth"
+            component={AuthRouter}
+          />
+          <Route
+            path="/"
+            exact
+            component={JournalScreen}
+          />
+        </Switch>
+      </div>
+    </Router>
+    )
+}
