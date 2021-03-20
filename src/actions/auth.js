b2721c2f51cad23faed2404e884deb47230534a4
Santigp258 <santigp258@gmail.com>
2021-03-20 14:12:59 -0500

    startLoginEmailPassword

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,5 +1,12 @@
 import { types } from "../types/types";
 
+export const startLoginEmailPassword = (email, password)=>{
+    return (dispatch)=> {
+        setTimeout(() => {
+            dispatch(login('santigclar', 1235));
+        }, 3000);
+    }
+}
 
 //return login action
 export const login = (uid, displayName) => ({
--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 
 import { Link } from "react-router-dom";
-import { login } from "../../actions/auth";
+import { startLoginEmailPassword } from "../../actions/auth";
 import { useForm } from "../../hooks/useForm";
 
 export const LoginScreen = () => {
@@ -21,7 +21,7 @@ export const LoginScreen = () => {
     e.preventDefault();
 
     //send action
-    dispatch(login(12345, 'Santiago Guerrero'));
+    dispatch(startLoginEmailPassword(email, password));
   };
   return (
     <>
